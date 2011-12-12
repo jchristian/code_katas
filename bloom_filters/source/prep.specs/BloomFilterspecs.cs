8515fa0e020cdfeefa27f87f54c2535acc465e6f
unknown <Jonathan@JonathanUberDT.(none)>
2011-12-11 20:02:39 -0800

    Implemented the bloom filter.

--- bloom_filters/source/prep.specs/BloomFilterspecs.cs
+++ bloom_filters/source/prep.specs/BloomFilterspecs.cs
@@ -5,33 +5,53 @@ using prep.bf;
 
 namespace prep.specs
 {
-  public class BloomFilterSpecs
-  {
-    public abstract class concern : Observes<ICheckIfIContainHashes,
-                                      BloomFilter>
+    public class BloomFilterSpecs
     {
-    }
-
-    [Subject(typeof(BloomFilter))]
-    public class when_checking_to_see_if_the_hashes_are_contained : concern
-    {
-
-      public class and_all_the_hashes_are_contained
-      {
-        Establish c = () =>
+        public abstract class concern : Observes<ICheckIfIContainHashes,
+                                            BloomFilter>
         {
-          depends_on<>();
-        };
+        }
 
-        Because of = () =>
-          are_all_the_hashes_contained = sut.contains(the_hashes);
+        [Subject(typeof(BloomFilter))]
+        public class when_checking_to_see_if_the_hashes_are_contained : concern
+        {
+            public class and_all_the_hashes_are_contained
+            {
+                Establish c = () =>
+                {
+                    the_hashes = new[] {1};
 
-        It should_return_true = () =>
-          are_all_the_hashes_contained.ShouldBeTrue();
+                    depends.on<IEnumerable<int>>(new[] {1});
+                };
 
-        static bool are_all_the_hashes_contained;
-        static IEnumerable<int> the_hashes;
-      }
+                Because of = () =>
+                    are_all_the_hashes_contained = sut.contains(the_hashes);
+
+                It should_return_true = () =>
+                    are_all_the_hashes_contained.ShouldBeTrue();
+
+                static bool are_all_the_hashes_contained;
+                static IEnumerable<int> the_hashes;
+            }
+
+            public class and_a_hash_is_not_contained
+            {
+                Establish c = () =>
+                {
+                    the_hashes = new[] { 1 };
+
+                    depends.on<IEnumerable<int>>(new[] { 2 });
+                };
+
+                Because of = () =>
+                    are_all_the_hashes_contained = sut.contains(the_hashes);
+
+                It should_return_false = () =>
+                    are_all_the_hashes_contained.ShouldBeFalse();
+
+                static bool are_all_the_hashes_contained;
+                static IEnumerable<int> the_hashes;
+            }
+        }
     }
-  }
 }
--- bloom_filters/source/prep/bf/BloomFilter.cs
+++ bloom_filters/source/prep/bf/BloomFilter.cs
@@ -1,12 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications.Utility;
 
 namespace prep.bf
 {
-  public class BloomFilter : ICheckIfIContainHashes
-  {
-    public bool contains(IEnumerable<int> hashes)
+    public class BloomFilter : ICheckIfIContainHashes
     {
-      throw new System.NotImplementedException();
+        const int filter_lenght = 10000;
+        BitArray filter;
+
+        public BloomFilter(IEnumerable<int> hashes)
+        {
+            initialize_filter(hashes);
+        }
+
+        void initialize_filter(IEnumerable<int> hashes)
+        {
+            filter = new BitArray(filter_lenght);
+            hashes.Each(x => filter[x%filter_lenght] = true);
+        }
+
+        public bool contains(IEnumerable<int> hashes)
+        {
+            return hashes.All(x => filter[x%10000]);
+        }
     }
-  }
 }
