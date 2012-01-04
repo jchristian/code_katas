e46edb3367ab79070547271a0afae52cc50d0a37
unknown <jchristian@CHRISTIANDT.BenchmarkPlus.local>
2012-01-04 09:38:40 -0800

    Added the initial decorating syntax.

--- method_decorator/UI/Program.cs
+++ method_decorator/UI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Windows.Forms;
 using UI.Presenters;
 using UI.Views;
@@ -16,8 +17,38 @@ namespace UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            var presenter = new ChatPresenter(new ChatView(), new CommunicationProxy());
+            var presenter = new ChatPresenter(new ChatView().Decorate(x => x.AddMessage(null, null)).With(InvokeRequired.Invoke), new CommunicationProxy());
             Application.Run(presenter.TheView.TheForm);
         }
+    }
+
+    public static class ObjectExtensions
+    {
+        public static IBuildADecorator<T> Decorate<T>(this T instance, Expression<Action<T>> methodToDecorate)
+        {
+            return new DecoratorBuilder<T>(methodToDecorate);
+        }
+    }
+
+    public class DecoratorBuilder<T> : IBuildADecorator<T>
+    {
+        Expression<Action<T>> _methodToDecorate;
+
+        public DecoratorBuilder(Expression<Action<T>> methodToDecorate)
+        {
+            _methodToDecorate = methodToDecorate;
+        }
+
+        public T With(Action<T> decoration)
+        {
+            _classBuilder.Build<T>()
+                .Substituting(_methodBuilder.Combine(decoration, _methodToDecorate.Compile()))
+                .For(_methodToDecorate);
+        }
+    }
+
+    public interface IBuildADecorator<T>
+    {
+        T With(Action<T> decoration);
     }
 }
