3b9c56512d3f5c19103211a88da9da12e7e54254
Jonathan Christian <jont.christian@gmail.com>
2013-01-22 11:04:22 -0800

    Started an implementation of a singly linked list.

---
+++ linked_list/src/console/LinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace console
+{
+    public class LinkedList<T> : IList<T>
+    {
+        Node<T> head;
+        Node<T> tail; 
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(item);
+                tail = head;
+            }
+            else
+            {
+                var old_tail = tail;
+                tail = new Node<T>(item);
+                old_tail.Next = tail;
+            }
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly { get; private set; }
+
+        public int IndexOf(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            var nodeAtPreviousIndex = ElementAt(index - 1);
+            var newNodeAtIndex = new Node<T>(item, nodeAtPreviousIndex.Next);
+            nodeAtPreviousIndex.Next = newNodeAtIndex;
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get { return ElementAt(index).Value; }
+            set { ElementAt(index).Value = value; }
+        }
+
+        Node<T> ElementAt(int index)
+        {
+            if (index >= Count)
+                throw new ArgumentException(null, "index");
+
+            var node = head;
+
+            for (int i = 0; i < index; i++)
+                node = node.Next;
+
+            return node;
+        }
+
+        class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+
+            public Node(T value) : this(value, null) { }
+            public Node(T value, Node<T> next)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+    }
+}
---
+++ linked_list/src/console/LinkedListSpecs.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.nsubstitue;
+
+namespace console
+{
+    public class LinkedListSpecs
+    {
+        public abstract class concern : Observes<IList<int>,
+                                            LinkedList<int>> { }
+
+        [Subject(typeof(LinkedList<int>))]
+        public class when_adding_to_an_empty_list : concern
+        {
+            Establish c = () =>
+                added_item = 1;
+
+            Because of = () =>
+                sut.Add(added_item);
+
+            It should_add_the_item_to_the_list = () =>
+                sut[0].ShouldEqual(added_item);
+
+            static int added_item;
+        }
+
+        [Subject(typeof(LinkedList<int>))]
+        public class when_adding_to_a_populated_list : concern
+        {
+            Establish c = () =>
+            {
+                added_item = 1;
+                sut_setup.run(x => x.Add(2));
+                sut_setup.run(x => x.Add(3));
+            };
+
+            Because of = () =>
+                sut.Add(added_item);
+
+            It should_add_the_item_to_the_end_of_the_list = () =>
+                sut[2].ShouldEqual(added_item);
+
+            static int added_item;
+        }
+
+        [Subject(typeof(LinkedList<int>))]
+        public class when_inserting_an_item_into_the_middle_of_a_list : concern
+        {
+            Establish c = () =>
+            {
+                added_item = 1;
+                previous_item_at_inserted_index = 3;
+                sut_setup.run(x => x.Add(2));
+                sut_setup.run(x => x.Add(previous_item_at_inserted_index));
+                sut_setup.run(x => x.Add(4));
+            };
+
+            Because of = () =>
+                sut.Insert(1, added_item);
+
+            It should_insert_the_item_into_the_correct_position = () =>
+                sut[1].ShouldEqual(added_item);
+
+            It should_move_the_item_previously_in_the_inserted_index_forward = () =>
+                sut[2].ShouldEqual(previous_item_at_inserted_index);
+
+            static int added_item;
+            static int previous_item_at_inserted_index;
+        }
+
+        [Subject(typeof(LinkedList<int>))]
+        public class when_inserting_an_item_to_the_end_of_the_list : concern
+        {
+            Establish c = () =>
+            {
+                added_item = 1;
+                sut_setup.run(x => x.Add(2));
+                sut_setup.run(x => x.Add(3));
+            };
+
+            Because of = () =>
+                sut.Insert(2, added_item);
+
+            It should_insert_the_item_into_the_correct_position = () =>
+                sut[2].ShouldEqual(added_item);
+
+            static int added_item;
+        }
+
+        [Subject(typeof(LinkedList<int>))]
+        public class when_inserting_an_item_outside_the_lists_size : concern
+        {
+            Establish c = () =>
+            {
+                added_item = 1;
+                sut_setup.run(x => x.Add(2));
+                sut_setup.run(x => x.Add(3));
+            };
+
+            Because of = () =>
+                spec.catch_exception(() => sut.Insert(3, added_item));
+
+            It should_throw_an_argument_exception_with_an_argument_of_index = () =>
+            {
+                spec.exception_thrown.ShouldBe(typeof(ArgumentException));
+                ((ArgumentException)spec.exception_thrown).ParamName.ShouldEqual("index");
+            };
+
+            static int added_item;
+        }
+    }
+}
