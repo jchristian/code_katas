e00aa18544472bc93e2e006a2b54a7970f810e38
Jonathan Christian <jont.christian@gmail.com>
2013-06-06 10:50:16 -0700

    Finished euler 9.

--- f_sharp/euler_9.fs
+++ f_sharp/euler_9.fs
@@ -10,9 +10,15 @@ let get_triplets =
             a_loop (inner_loop acc a (1000 - (a * 2))) (a + 1)
     a_loop [] 0
 
+let does_sum_equal x = function
+    | (a, b, c) -> a + b + c = 1000
+
+
 let pythagorean_triplet_sum_equal_to_1000 =
     let rec loop = function
-        |
+        | head :: tail when (is_pythagorean_triplet head) && (does_sum_equal 1000 head) -> head
+        | head :: tail -> loop tail
+    loop (get_triplets)
 
 //Testing functions
 let pass_fail = function
@@ -27,4 +33,7 @@ run_test "is_pythagorean_triplet 1 2 3" false (is_pythagorean_triplet (1, 2, 3))
 run_test "is_pythagorean_triplet 3 4 5" true (is_pythagorean_triplet (3, 4, 5))
 run_test "is_pythagorean_triplet 4 4 5" false (is_pythagorean_triplet (4, 4, 5))
 
-printfn "%A" get_triplets
+run_test "does_sum_equal 1000 (100, 200, 300)" false (does_sum_equal 1000 (100, 200, 300))
+run_test "does_sum_equal 1000 (300, 300, 400)" true (does_sum_equal 1000 (300, 300, 400))
+
+printfn "Pythagorean triplet whose sum equals 1000: %A" (pythagorean_triplet_sum_equal_to_1000)
