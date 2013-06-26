964bf7273d70b9090cd0e0169a991fa129947c76
Jonathan Christian <jont.christian@gmail.com>
2013-06-26 10:56:58 -0700

    More work on euler 18.

--- f_sharp/euler_18.fs
+++ f_sharp/euler_18.fs
@@ -6,10 +6,19 @@ let parse_to_int (str : string) =
     match System.Int32.TryParse(str) with
     | (false, _) -> None
     | (true, value) -> Some(value)
-let print_and_pass x = printfn "%A" x; x;
+let print x = printfn "%A" x; x;
 
-let find_maximum_sum_path triangle = 0
-let load_triangle (str : string) = str |> split '\n' |> print_and_pass |> (List.map (fun x -> x |> split ' ' |> List.choose parse_to_int))
+let find_maximum_sum_path (triangle : List<List<int>>)=
+    let rec generate_next_row previous_row previous_cell acc = function
+        | [] -> acc
+        | head :: tail ->
+            let next_cell = if List.isEmpty previous_row then 0 else previous_row.Head
+            let max_cell = if next_cell > previous_cell then next_cell else previous_cell
+            generate_next_row (previous_row.Tail) next_cell (max_cell + head :: acc) tail
+    let last_row = //triangle |> List.fold (fun previous_row current_row -> generate_next_row previous_row 0 [] current_row)
+    last_row |> List.fold (fun x y -> if x > y then x else y) 0
+
+let load_triangle (str : string) = str |> split '\n' |> print |> (List.map (fun x -> x |> split ' ' |> List.choose parse_to_int))
 
 //Testing functions
 let pass_fail = function
@@ -35,7 +44,7 @@ let example_triangle = "
 2 4 6
 8 5 9 3"
 
-run_test "find_maximum_sum_path example_triangle" 23 (example_triangle |> load_triangle |> print_and_pass |> find_maximum_sum_path)
+run_test "find_maximum_sum_path example_triangle" 23 (example_triangle |> load_triangle |> find_maximum_sum_path)
 
 let main = printfn ""
 
--- f_sharp/scratch_pad.fs
+++ f_sharp/scratch_pad.fs
@@ -1,19 +1,4 @@
-let withSideEffects x =
-    x := "Side Effected"
-
-let main() =
-    let msg = ref "Not Effected"
-    printfn "%A" !msg
-
-    let setMsg () =
-        msg := "set"
-    setMsg()
-    printfn "%A" !msg
-
-    withSideEffects msg
-    printfn "%A" !msg
-
-main()    
+printfn "%A" ([].Head)
 
 //Testing functions
 let pass_fail = function
