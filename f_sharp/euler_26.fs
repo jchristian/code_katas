a9c4ea691ff71e797863c6bf7f2eb904427fefc4
Jonathan Christian <jont.christian@gmail.com>
2013-07-12 10:48:02 -0700

    First cut at euler 26.

---
+++ f_sharp/euler_26.fs
@@ -0,0 +1,30 @@
+open System
+open Helpers.Testing
+open Helpers.Math.Core
+
+let recurring_cycle_size x =
+    let rec loop acc = function
+        | 0 -> 0
+        | remainder ->
+            let current = (remainder * 10) / x
+            let next_remainder = (remainder * 10) % x
+            if acc |> List.exists (fun y -> y = current) then
+                acc |> List.findIndex (fun y -> y = current) |> (fun x -> x + 1)
+            else
+                loop (current :: acc) next_remainder
+    loop [] 1
+
+run_test "recurring_cycle_size 2" 0 (recurring_cycle_size 2)
+run_test "recurring_cycle_size 3" 1 (recurring_cycle_size 3)
+run_test "recurring_cycle_size 4" 0 (recurring_cycle_size 4)
+run_test "recurring_cycle_size 6" 1 (recurring_cycle_size 6)
+run_test "recurring_cycle_size 7" 6 (recurring_cycle_size 7)
+run_test "recurring_cycle_size 11" 2 (recurring_cycle_size 11)
+run_test "recurring_cycle_size 13" 6 (recurring_cycle_size 13)
+run_test "recurring_cycle_size 81" 9 (recurring_cycle_size 81)
+
+let main =
+    time_and_save (fun() -> [2 .. 1000] |> List.map (fun x -> (recurring_cycle_size x, x)) |> List.sortBy (fun (s, i) -> s) |> List.rev |> List.head |> snd)
+
+[<STAThread>]
+do main
