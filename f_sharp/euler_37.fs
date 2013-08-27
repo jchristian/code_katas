9bff9a634a7720c83730b6377ebb77a842cd7f2e
Jonathan Christian <jont.christian@gmail.com>
2013-08-27 13:34:51 -0700

    Improved euler 37.

--- f_sharp/euler_37.fs
+++ f_sharp/euler_37.fs
@@ -21,24 +21,23 @@ let primes_up_to x =
     loop (highest_prime + 1)
     primes
 
-let get_root x = int (Math.Sqrt((double x))) + 1
+let get_root x = int (Math.Sqrt((double x)))
 
-let is_prime x =
-    if x < 2 then
-        false
-    else
-        let the_primes = primes_up_to (get_root x)
-        is_x_divisible_by x primes |> not
+let is_prime n =
+    match n with
+    | n when n <= 1 -> false 
+    | _ ->
+        let rec check_prime number check =
+            match check with
+            | 1 | 0 -> true
+            | _ -> not (number % check = 0) && check_prime number (check - 1)
+        check_prime n (get_root n)
 
-let convert_list_of_digits_to_number list = list |> List.rev |> List.mapi (fun i x -> x * (pown 10 i)) |> List.fold (+) 0
+let memoized_is_prime = memoize is_prime
 
-let remove_left_digit x =
-    let digits = x |> get_digits
-    digits |> List.tail |> convert_list_of_digits_to_number
-
-let remove_right_digit x =
-    let digits = x |> get_digits
-    digits |> List.rev |> List.tail |> List.rev |> convert_list_of_digits_to_number
+let to_int_zero_if_empty s = if Seq.length s = 0 then 0 else int s
+let remove_left_digit x = (x.ToString()) |> (fun s -> s.[1 ..]) |> to_int_zero_if_empty
+let remove_right_digit x = (x.ToString()) |> (fun s -> s.[..(Seq.length s - 2)]) |> to_int_zero_if_empty
 
 let is_truncatably_prime x =
     if x < 10 then
@@ -47,7 +46,7 @@ let is_truncatably_prime x =
         let rec loop truncate = function
             | 0 -> true
             | x ->
-                if x |> is_prime |> not then
+                if x |> memoized_is_prime |> not then
                     false
                 else
                     loop truncate (truncate x)
@@ -58,11 +57,11 @@ run_test "is_prime 1" false (is_prime 1)
 run_test "is_prime 2" true (is_prime 2)
 run_test "is_prime 4" false (is_prime 4)
 run_test "is_prime 7" true (is_prime 7)
+run_test "is_prime 37" true (is_prime 37)
+run_test "is_prime 73" true (is_prime 73)
 run_test "is_prime 103" true (is_prime 103)
 run_test "is_prime 117" false (is_prime 117)
 run_test "is_prime 118" false (is_prime 118)
-
-run_test "convert_list_of_digits_to_number [1; 2; 3; 4]" 1234 (convert_list_of_digits_to_number [1; 2; 3; 4])
 
 run_test "remove_left_digit 1234" 234 (remove_left_digit 1234)
 run_test "remove_right_digit 1234" 123 (remove_right_digit 1234)
@@ -84,12 +83,12 @@ let main =
                 | head :: tail ->
                     let potential_match = cur * 10 + head
                     if is_truncatably_prime potential_match then
-                        (potential_match :: (loop potential_match [0 .. 9])) @ (loop cur tail)
-                    else if is_prime potential_match then
-                        (loop potential_match [0 .. 9]) @ loop cur tail
+                        (potential_match :: (loop potential_match [1; 3; 5; 7; 9])) @ (loop cur tail)
+                    else if memoized_is_prime potential_match then
+                        (loop potential_match  [1; 3; 5; 7; 9]) @ loop cur tail
                     else
                         loop cur tail
-            [2; 3; 5; 7] |> List.map (fun elem -> loop elem [0 .. 9]) |> List.reduce (@)
+            [2; 3; 5; 7] |> List.map (fun elem -> loop elem  [1; 3; 5; 7; 9]) |> List.reduce (@)
         get_all_truncatably_primes |> List.reduce (+))
 
 [<STAThread>]
