cf6034fdf4039b8f5478c339b7d428f7bef92ac8
Jonathan Christian <jont.christian@gmail.com>
2013-10-14 11:04:28 -0700

    Added a solution for euler 44.

--- f_sharp/euler_44.fs
+++ f_sharp/euler_44.fs
@@ -8,42 +8,24 @@ let pentagon_numbers =
   let rec pentagon_numbers_after x = seq { yield pentagon_number x; yield! pentagon_numbers_after (x + 1) }
   pentagon_numbers_after 1
 
-let move_next (p_i, p_j, i, j, diff) = (p_i, pentagon_number (j + 1), i, j + 1, ((pentagon_number (j + 1)) - p_i))
-let first (x, _, _, _, _) = x
-let second (_, x, _, _, _) = x
-let third (_, _, x, _, _) = x
+let pentagonal_index x = (1.0 + (Math.Sqrt((float x) * 24.0 + 1.0))) / 6.0
+let is_pentagonal x = (pentagonal_index x) = (float (int (pentagonal_index x)))
+let are_sum_and_difference_pentagonal (pi, pj) = printfn "%A %A %A %A" pi pj (pj - pi) (pi + pj); is_pentagonal (pj - pi) && is_pentagonal (pi + pj)
 
-let pentagon_pairs_with_differences_minimized =
-  let rec loop rows = seq {
-    let row_with_min_difference = rows |> Array.minBy (fun (_, _, _, _, diff) -> diff)
-    let diff = row_with_min_difference |> (fun (_, _, _, _, diff) -> diff)
+let pentagonal_pairs_with_difference_of x =
+    pentagon_numbers |> Seq.takeWhile ((>) (x / 2)) |> Seq.choose (fun p -> if is_pentagonal (x - p) then Some (p, (x - p)) else None)
 
-    let diff_of_next_row = ((rows |> Array.length) + 1) * 3 + 1
-    let should_add_new_row = diff > diff_of_next_row
-    if should_add_new_row then
-      let i = Array.length rows + 1
-      let j = i + 1
-      let pent_i = pentagon_number i
-      let pent_j = pentagon_number j
-      let next_row = (pent_i, pent_j, i, j, pent_j - pent_i)
-      let new_rows = rows |> Array.append [|move_next next_row|]
-      yield (first next_row, second next_row)
-      yield! loop new_rows
-    else
-      let i = rows |> Array.findIndex (fun row -> (third row = third row_with_min_difference))
-      yield (first (rows.[i]), second (rows.[i]))
-      rows.[i] <- move_next (rows.[i])
-      yield! loop rows
-  }
-
-  let pent_1 = pentagon_number 1
-  let pent_2 = pentagon_number 2
-  loop [|(pent_1, pent_2, 1, 2, pent_2 - pent_1)|]
-
-run_test "Seq.take 9 pentagon_pairs_with_differences_minimized" [(1, 5); (5, 12); (12, 22); (1, 12); (22, 35); (35, 51); (5, 22); (51, 70); (1, 22);] (Seq.take 9 pentagon_pairs_with_differences_minimized |> Seq.toList)
+run_test "is_pentagonal 1" true (is_pentagonal 1)
+run_test "is_pentagonal 5" true (is_pentagonal 5)
+run_test "is_pentagonal 50" false (is_pentagonal 50)
+run_test "is_pentagonal 51" true (is_pentagonal 51)
+run_test "is_pentagonal 52" false (is_pentagonal 52)
+run_test "is_pentagonal 144" false (is_pentagonal 144)
+run_test "is_pentagonal 145" true (is_pentagonal 145)
+run_test "is_pentagonal 146" false (is_pentagonal 146)
 
 let main =
-    time_and_save (fun() -> 1)
+    time_and_save (fun() -> pentagon_numbers |> Seq.collect pentagonal_pairs_with_difference_of |> Seq.find are_sum_and_difference_pentagonal |> (fun (pk, pj) -> pj - pk))
 
 [<STAThread>]
 do main
